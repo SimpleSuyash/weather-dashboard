891ba8c2cf768067cf0af978a5e7e3c75e584f32
SimpleSuyash <suyashmaharjan@gmail.com>
2023-12-21 23:21:19 +1100

    UI changed

--- src/script/script.js
+++ src/script/script.js
@@ -1,41 +1,39 @@
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+dayjs.extend(window.dayjs_plugin_utc);
 
 
-async function initMap() {
     const searchErrorMsgEl = document.getElementById("error");
     const searchBtnEl= document.getElementById('search-btn');
     const searchInputEl = document.getElementById('search-input');
     const currentWeatherEl = document.getElementById('current-weather');
     const citiesEl = document.getElementById('cities');
     const forecastEl = document.getElementById('forecast-row');
-    const key = "5be9f109a7d5fab8bbaf3f512f90f09c";
     let coordinates;
-    let place;
+    
    // https://jsfiddle.net/gh/get/library/pure/googlemaps/js-samples/tree/master/dist/samples/places-autocomplete/jsfiddle
-    const options = {
-        fields: ["formatted_address", "geometry", "name"],
-        strictBounds: false,
-      };
-    const autocomplete = new google.maps.places.Autocomplete(searchInputEl, options);
+
+    const autocomplete = new google.maps.places.Autocomplete(searchInputEl);
     autocomplete.addListener("place_changed", () => {
-        place = autocomplete.getPlace();
+        const place = autocomplete.getPlace();
         if (!place.geometry || !place.geometry.location) {
             // User entered the name of a Place that was not suggested and
             // pressed the Enter key, or the Place Details request failed.
-            searchInputEl.value="";
             searchErrorMsgEl.innerHTML =`No city is found for input: <span class="fst-italic text-danger"> ${place.name}</span>`;
             searchErrorMsgEl.hidden=false;
             setTimeout(() => searchErrorMsgEl.hidden=true, 3000);
+            searchInputEl.value="";
             return;
-          }
-          else{
+            }
+            else{
             coordinates = place.geometry.location;
-          }
+            }
     });
 
-
-    function showCurrentWeather(){
-        const today = dayjs().format(` (D/M/YYYY)`);
-        currentWeatherEl.innerHTML =`<h2 class="fw-bold">${place.name} ${today}<i></i></h2>`;
+    function showCurrentWeather(data){
+        const city = data.name;
+        const date = dayjs.unix(data.dt).format("ddd, Do [of] MMM, YYYY h:ma");
+        // const date = dayjs.unix(data.dt).format("ddd, Do [of] MMM, YYYY h:ma");
+        currentWeatherEl.innerHTML =`<h2 class="fw-bold">${city}</h2> ${date}<i></i>`;
     }
 
                  // <h2 class="fw-bold">Sydney 19/12/2023<i></i></h2>
@@ -43,33 +41,34 @@ async function initMap() {
                 //     <p>Wind: 8.43 MPH</p>
                 //     <p>Humidity: 44%</p>
 
-    function searchCity(){
-        
+    function getCityWeather(){
+        // const url= "http://api.openweathermap.org/geo/1.0/direct?";
+        const url= "https://api.openweathermap.org/data/2.5/forecast?";
+        const key = "5be9f109a7d5fab8bbaf3f512f90f09c";
         if(coordinates){
-            
-            const requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat()}&lon=${coordinates.lng()}&appid=${key}`;
+            const requestUrl = `${url}lat=${coordinates.lat()}&lon=${coordinates.lng()}&units=metric&appid=${key}`;
+            // const requestUrl = `${url}q=Kathmandu&units=metric&appid=${key}`;
             fetch(requestUrl)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
                 console.log(data);
-
-                showCurrentWeather();
-
-   
+                showCurrentWeather(data);
+                searchInputEl.value ="";
             })
             .catch(function (err) {
                 console.log("Something went wrong!", err);
             });
-        }else{}
-        
-
+        }else{
+            //when user typed a city and that didn't show in google autocomplet but user still pressed search button
+            searchErrorMsgEl.innerHTML =`No city is found for input: <span class="fst-italic text-danger"> ${searchInputEl.value}</span>`;
+            searchErrorMsgEl.hidden=false;
+            setTimeout(() => searchErrorMsgEl.hidden=true, 3000);
+            searchInputEl.value="";
+        }
     }
 
-    searchBtnEl.addEventListener('click', searchCity);
+    searchBtnEl.addEventListener('click', getCityWeather);
 
 
-}
-
- initMap();
